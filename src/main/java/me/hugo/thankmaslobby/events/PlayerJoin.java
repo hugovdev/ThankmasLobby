38f26b24e9b0c7afae47eb3bf06e3217c7ecbfba
Hugo <hugov2002@gmail.com>
2021-12-28 15:07:25 +0100

    Added some nice comments

--- src/main/java/me/hugo/thankmaslobby/events/PlayerJoin.java
+++ src/main/java/me/hugo/thankmaslobby/events/PlayerJoin.java
@@ -8,7 +8,6 @@ import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextColor;
 import net.kyori.adventure.text.format.TextDecoration;
 import net.kyori.adventure.title.TitlePart;
-import net.kyori.adventure.util.RGBLike;
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.advancements.FrameType;
 import net.minestom.server.advancements.notifications.Notification;
@@ -18,25 +17,27 @@ import net.minestom.server.entity.*;
 import net.minestom.server.event.GlobalEventHandler;
 import net.minestom.server.event.player.PlayerLoginEvent;
 import net.minestom.server.event.player.PlayerSpawnEvent;
+import net.minestom.server.inventory.condition.InventoryCondition;
 import net.minestom.server.item.ItemStack;
 import net.minestom.server.item.Material;
 
 public class PlayerJoin {
 
     Notification joinNotification;
+    InventoryCondition inventoryConditionResult;
 
     public PlayerJoin(GlobalEventHandler globalEventHandler) {
         ThankmasLobby thankmasLobby = ThankmasLobby.getInstance();
-        joinNotification = new Notification(
-                Component.text("Log into ", NamedTextColor.WHITE).append(Component.text("Hytale Thankmas 2022").color(NamedTextColor.AQUA)),
-                FrameType.TASK,
-                ItemStack.of(Material.GOLD_NUGGET)
-        );
+
+        initInventoryConditionResult();
+        initJoinNotification();
 
         globalEventHandler.addListener(PlayerLoginEvent.class, event -> {
             final Player player = event.getPlayer();
+            // Generate GamePlayer class and load data
             thankmasLobby.getPlayerManager().getPlayerData(player);
 
+            // Set player spawn location
             event.setSpawningInstance(MinecraftServer.getInstanceManager().getInstances().iterator().next());
             player.setRespawnPoint(new Pos(-0.5, 47, -4.5));
         });
@@ -44,32 +45,54 @@ public class PlayerJoin {
         globalEventHandler.addListener(PlayerSpawnEvent.class, event -> {
             final Player player = event.getPlayer();
 
+            // Send notifications, create scoreboard and send titles and subtitles
             NotificationCenter.send(joinNotification, player);
             thankmasLobby.getPlayerManager().getPlayerData(player).loadSidebar();
 
             player.sendTitlePart(TitlePart.TITLE, Component.text("Hytale Thankmas", TextColor.color(0xFFB400)).decorate(TextDecoration.BOLD));
             player.sendTitlePart(TitlePart.SUBTITLE, Component.text("Year 2022", TextColor.color(0xFFD358)));
 
-            player.getInventory().addInventoryCondition((player1, i, clickType, inventoryConditionResult) -> {
-                if (player1.getOpenInventory() == null) {
-                    HotBarItem hotBarItem = HotBarItem.fromItem(inventoryConditionResult.getClickedItem());
+            // Give hotbar items and update everyone's player counter
+            giveHotbarItems(player);
+            updatePlayerCounter();
 
-                    if (hotBarItem != null) {
-                        hotBarItem.getClickAction().execute(player);
-                    }
-                }
-                inventoryConditionResult.setCancel(true);
-            });
-
-            for (HotBarItem hotBarItem : HotBarItem.values())
-                player.getInventory().setItemStack(hotBarItem.getSlot(), hotBarItem.getItem());
-
-            int onlinePlayers = MinecraftServer.getConnectionManager().getOnlinePlayers().size();
-
-            for (GamePlayer gamePlayer : ThankmasLobby.getInstance().getPlayerManager().getPlayerStorage().values()) {
-                gamePlayer.getScoreboard().updateLineContent("lobbyPlayerCounter", Component.text("Players: ").color(NamedTextColor.WHITE)
-                        .append(Component.text(onlinePlayers).color(NamedTextColor.GREEN)));
-            }
+            // Set up player's inventory for clicks
+            player.getInventory().addInventoryCondition(this.inventoryConditionResult);
         });
     }
+
+    private void giveHotbarItems(Player player) {
+        for (HotBarItem hotBarItem : HotBarItem.values())
+            player.getInventory().setItemStack(hotBarItem.getSlot(), hotBarItem.getItem());
+    }
+
+    private void updatePlayerCounter() {
+        int onlinePlayers = MinecraftServer.getConnectionManager().getOnlinePlayers().size();
+
+        for (GamePlayer gamePlayer : ThankmasLobby.getInstance().getPlayerManager().getPlayerStorage().values())
+            gamePlayer.getScoreboard().updateLineContent("lobbyPlayerCounter",
+                    Component.text("Players: ").color(NamedTextColor.WHITE).append(Component.text(onlinePlayers).color(NamedTextColor.GREEN)));
+    }
+
+    private void initJoinNotification() {
+        this.joinNotification = new Notification(
+                Component.text("Log into ", NamedTextColor.WHITE).append(Component.text("Hytale Thankmas 2022").color(NamedTextColor.AQUA)),
+                FrameType.TASK,
+                ItemStack.of(Material.GOLD_NUGGET)
+        );
+    }
+
+    private void initInventoryConditionResult() {
+        this.inventoryConditionResult = ((player, i, clickType, inventoryConditionResult) -> {
+            if (player.getOpenInventory() == null) {
+                HotBarItem hotBarItem = HotBarItem.fromItem(inventoryConditionResult.getClickedItem());
+
+                if (hotBarItem != null) {
+                    hotBarItem.getClickAction().execute(player);
+                }
+            }
+            inventoryConditionResult.setCancel(true);
+        });
+    }
+
 }
